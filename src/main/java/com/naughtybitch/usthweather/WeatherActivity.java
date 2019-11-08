6a8fb10414361dd0c169b1ff29367b013bd5f827
Phong Vu <phongvu99@outlook.com>
2019-11-08 15:20:50 +0700

    Second Practical Work

---
+++ src/main/java/com/naughtybitch/usthweather/WeatherActivity.java
@@ -0,0 +1,46 @@
+package com.naughtybitch.usthweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Log.i("create_tag", "Creating...");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("start_tag","Starting...");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("resume_tag","Resuming...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("pause_tag","Pausing...");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("stop_tag","Stopping...");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("destroy_tag","Destroying...");
+    }
+}
