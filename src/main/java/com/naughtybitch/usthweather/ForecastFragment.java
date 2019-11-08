3d55441dbd16b009b77e120e1b9a62f317dcfa49
Phong Vu <phongvu99@outlook.com>
2019-11-08 16:16:51 +0700

    Problematic guy here

---
+++ src/main/java/com/naughtybitch/usthweather/ForecastFragment.java
@@ -0,0 +1,28 @@
+package com.naughtybitch.usthweather;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ForecastFragment extends Fragment {
+    public ForecastFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = new View(getContext());
+        v.setBackgroundColor(0xFF0000);
+        return v;
+    }
+
+}
--- src/main/java/com/naughtybitch/usthweather/WeatherActivity.java
+++ src/main/java/com/naughtybitch/usthweather/WeatherActivity.java
@@ -1,9 +1,12 @@
 package com.naughtybitch.usthweather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -12,35 +15,43 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         Log.i("create_tag", "Creating...");
+
+        // Create a new fragment to be placed in the activity !
+        ForecastFragment firstFragment = new ForecastFragment();
+
+        // Add the fragment to the container LinearLayout
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, firstFragment).commit();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i("start_tag","Starting...");
+        Log.i("start_tag", "Starting...");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i("resume_tag","Resuming...");
+        Log.i("resume_tag", "Resuming...");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i("pause_tag","Pausing...");
+        Log.i("pause_tag", "Pausing...");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i("stop_tag","Stopping...");
+        Log.i("stop_tag", "Stopping...");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i("destroy_tag","Destroying...");
+        Log.i("destroy_tag", "Destroying...");
     }
+
 }
